479ccc5cbbe8cb7cbca802e29492c7f84e4f0809
khushi08 <84787614+khushi-stack@users.noreply.github.com>
2024-03-08 23:38:08 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+def even_odd(x):
+    if(x % 2 == 0):
+        print(f"The number {x} is even")
+    else:
+        print(f"The number {x} is odd")
+
+
+x = int(input("Enter the number"))
+
+even_odd(x)        
