52bcced0b2e05ebec8d44d8aabd84c3a35c59a76
khushi08 <84787614+khushi-stack@users.noreply.github.com>
2024-03-14 23:30:04 +0530

    Add files via upload

---
+++ vowel_consonant.py
@@ -0,0 +1,12 @@
+def vowelOrConsonant(x): 
+  
+    if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'): 
+        print("Vowel") 
+    else: 
+        print("Consonant") 
+  
+# Driver code 
+
+print(vowelOrConsonant('e'))
+print(vowelOrConsonant('f'))
+
