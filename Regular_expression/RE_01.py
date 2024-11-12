b48650a008edd2a9f1729865c1d9410b3c612603
khushi08 <84787614+khushi-stack@users.noreply.github.com>
2024-11-12 06:37:34 +0000

    config done

--- Regular_expression/RE_01.py
+++ Regular_expression/RE_01.py
@@ -1,3 +1,14 @@
+# import re
+
+# string = "quick is a fox"
+# pattern = r"quick"
+
+# match = re.match(pattern,string)
+# if match:
+#     print("Match found:" , match.group())
+# else:
+#     print("no match")
+
 import re
 
 string = "this is a quick fox"
